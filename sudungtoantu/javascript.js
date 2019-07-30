15b64aa6058fafad27a758a0f6864d94860bee62
= <=>
2019-07-30 16:59:54 +0700

    sudungtoantu

---
+++ sudungtoantu/javascript.js
@@ -0,0 +1,13 @@
+
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("nhập chiều rộng:");
+inputHeight = prompt("nhập chiều cao:");
+
+let inputHeightok = parseInt(inputHeight);
+let inputWidthok = parseInt(inputWidth);
+let dientich = inputHeightok * inputWidthok
+
+
+document.write("tổng diện tích hình chữ nhật:" + dientich);
